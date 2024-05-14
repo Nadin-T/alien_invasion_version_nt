ece964f777f7936b4e484020e889d558bcbeb2e7
unknown <nadezhda.tarasova8@ya.ru>
2024-05-14 12:06:18 +0300

    add settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    #Класс для хранения всех настроек игры Alien Invasion.
+
+    def __init__(self):
+        #Инициализирует настройки игры.
+        #Параметры экрана.
+        self.screen_width = 1920
+        self.screen_heught = 1080
+        self.bg_color = (230, 230, 230)
